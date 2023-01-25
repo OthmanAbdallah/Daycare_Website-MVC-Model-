fd44a5613a0fd25e68b843276716f84e7734d34f
OthmanAbdallah <othmanabdallah2001@gmail.com>
2023-01-25 16:54:06 +0300

    phase 1

---
+++ Daycare Project(MVC Model)/project/Models/Register.cs
@@ -0,0 +1,13 @@
+﻿namespace project.Models
+{
+    public class Register
+    {
+        public string GName { get; set; }
+        public string Email { get; set; }
+        public string FNumber { get; set; }
+        public string CName { get; set; }
+        public string Gender { get; set; }
+        public string BirthDate { get; set; }
+        public string address { get; set; }
+    }
+}
